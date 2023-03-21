0335a1120052ea8a1b311f7986ddee2f0a31ba45
bruno pinho <brunopinho@outlook.com>
2023-03-21 19:15:13 -0300

    feat: create Chat component

---
+++ frontend/src/components/Chat/Chat.js
@@ -0,0 +1,6 @@
+const Chat = () => {
+  // TODO: remove chat logic from technician and attendant page and insert here
+  <h1>Chat</h1>
+};
+
+export default Chat;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as MessageTextField } from './MessageTextField';
 export { MessagesList } from './MessagesList';
+export { Chat } from './Chat';
